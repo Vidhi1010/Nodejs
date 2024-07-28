951661b1d274c679159ee3e6e6c71ec6c915d970
Vidhi1010 <vidhisinghvp1010@gmail.com>
2024-07-28 23:30:04 +0530

    modules in nodeJs

--- hello.js
+++ hello.js
@@ -1,2 +1,9 @@
-console.log("Hey there! I am JS") ;
+const math = require("./math")
+//const {add, sub} = require("./math")
 
+console.log("Hey there! I am JS");
+ 
+
+//console.log("Math Value is", math.addFn(4, 1) );
+
+console.log("Math Value is", math.add );
