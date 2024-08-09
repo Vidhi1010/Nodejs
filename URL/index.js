3184c12dec63db6c3cd0720f0719d7f87f7f736d
Vidhi1010 <vidhisinghvp1010@gmail.com>
2024-08-09 15:54:47 +0530

    updations

--- URL/index.js
+++ URL/index.js
@@ -3,12 +3,11 @@
 
 //https- Protocal: Hyper transfer protocal secure
 
-//www.vidhisingh.dev (Domain) - User Friendly Name of IP Address og my Server 
+//www.vidhisingh.dev (Domain) - User Friendly Name of IP Address og my Server
 
 // /- root
 
 // /about - path
-
 
 const http = require("http");
 const fs = require("fs");
@@ -18,19 +17,30 @@ const myServer = http.createServer((req, res) => {
   const log = `${Date.now()}: ${req.url} New Req Recieved \n`;
   const myUrl = url.parse(req.url, true);
   console.log(myUrl);
+
   fs.appendFile("log.txt", log, (err, data) => {
-    switch ((myUrl.pathname)) {
+    switch (myUrl.pathname) {
       case "/":
-        res.end("HomePage");
+        if (req.method === "GET") res.end("HomePage");
         break;
       case "/about":
-        const username = myUrl.query.myname
+        const username = myUrl.query.myname;
         res.end(`Hi, ${username}`);
+        break;
+      case "/search":
+        const search = myUrl.query.search_query;
+        res.end("Here are your results for " + search);
+        break;
+      case "/signup":
+        if(req.method === "GET") res.end("This is a signup form");
+        else if(req.method === "POST"){
+          res.end("Success");
+        }
         break;
       case "/contact":
         res.end("vidhi123@gmail.com");
         break;
-      default: 
+      default:
         res.end("404 Not Found");
     }
     //res.end("Hello From Server Again");
